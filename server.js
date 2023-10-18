1ed5a8a61c772bca5711fe7bdc591673748adff9
HHB <bcl0206@gmail.com>
2023-10-18 22:12:40 +0900

    feat: s3업로드 로직 구현

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { generateUploadURL } from './s3.js'
+
+const app = express()
+
+app.use(express.static('front'))
+
+app.get('/s3Url', async (req, res) => {
+  const url = await generateUploadURL()
+  res.send({url})
+})
